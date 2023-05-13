2bbe17c004c1c50ff6b4653757257e5913098633
Your Name <you@example.com>
2023-05-13 15:12:06 +0530

    Console in JS

--- 1/js/app.js
+++ 1/js/app.js
@@ -1,1 +1,29 @@
-alert("Hello")
+// console.log("Hello world")
+
+// console.log(3);
+// console.log(4.5);
+// console.log(true);
+
+// console.log(4);
+// console.error("Some error occured");
+// console.warn("You forgot a  semi colon");
+
+// console.clear();
+
+// console.log([1,2,3,4,5,6])
+
+
+//Primitive data types
+//Number
+//String
+//boolean
+//Undefined
+// Null
+// Symbol
+
+//primitive data types are stored in stacks
+
+
+
+//Non primitive  
+ 
